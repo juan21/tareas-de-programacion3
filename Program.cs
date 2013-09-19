1e33ec2bfd7d0c4491e397b9a7712e12f1c43fb3
juan21 <juan.corona@alumno.udg.mx>
2013-09-18 19:41:08 -0500

    tarea1

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Juan Carlos
+ * Date: 9/18/2013
+ * Time: 10:43 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace tarea1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			persona p= new persona();
+		Console.WriteLine ("\nnombre:");
+		p.nombre=Console.ReadLine();
+		Console.WriteLine("\nTu nombre es:"+p.nombre);
+		
+		Console.WriteLine("\ncorreo:");
+		p.correo=Console.ReadLine();
+		Console.WriteLine("\nTu correo es:"+p.correo);
+		
+		Console.WriteLine("\nedad:");
+	    p.edad=Console.ReadLine();
+		Console.WriteLine("\nTu edad es:"+p.edad);
+		
+			Console.Write("\nPresione cualquier tecla para continuar . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ persona.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Juan Carlos
+ * Date: 9/18/2013
+ * Time: 10:43 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace tarea1
+{
+	/// <summary>
+	/// Description of persona.
+	/// </summary>
+	public class persona
+	{
+		
+		public string nombre;                       
+		public string correo;
+		public string edad;
+		public persona()
+		{
+		}
+	}
+}
